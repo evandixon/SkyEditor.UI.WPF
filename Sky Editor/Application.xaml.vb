d599f5537c7916c2aaec73e8a2d19159b0366042
Evan Dixon <evanldixon@live.com>
2016-08-18 20:18:38 -0500

    Fixed threading deadlock

--- Sky Editor/Application.xaml.vb
+++ Sky Editor/Application.xaml.vb
@@ -3,8 +3,8 @@
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
 
-    Private Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
-        SkyEditor.UI.WPF.StartupHelpers.RunWPFStartupSequence()
+    Private Async Sub Application_Startup(sender As Object, e As StartupEventArgs) Handles Me.Startup
+        Await SkyEditor.UI.WPF.StartupHelpers.RunWPFStartupSequence()
     End Sub
 
 End Class
