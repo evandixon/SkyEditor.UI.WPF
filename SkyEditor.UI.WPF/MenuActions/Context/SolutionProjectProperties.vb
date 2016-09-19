96dd63001922b2232a088d7bbadfe68dbc1e781f
Evan Dixon <evanldixon@live.com>
2016-09-19 18:49:46 -0500

    Fixed SolutionProjectProperties opening solution

--- SkyEditor.UI.WPF/MenuActions/Context/SolutionProjectProperties.vb
+++ SkyEditor.UI.WPF/MenuActions/Context/SolutionProjectProperties.vb
@@ -10,8 +10,12 @@ Namespace MenuActions
             For Each item In Targets
                 If TypeOf item Is SolutionHeiarchyItemViewModel Then
                     Dim target As SolutionHeiarchyItemViewModel = item
-                    If Not target.IsDirectory Then
+                    If target.IsRoot Then
+                        'Open the solution
                         CurrentPluginManager.CurrentIOUIManager.OpenFile(target.Project, False)
+                    ElseIf Not target.IsDirectory Then
+                        'Open the selected project
+                        CurrentPluginManager.CurrentIOUIManager.OpenFile(target.GetNodeProject, False)
                     End If
                 End If
             Next
