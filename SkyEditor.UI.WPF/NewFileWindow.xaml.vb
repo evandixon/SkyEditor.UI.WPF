43721854d5539c35625e89444e4cd71ab01410b7
Evan Dixon <evanldixon@live.com>
2017-03-08 13:52:11 -0600

    Prevented NewFileWindow from yielding null name or type Close #21

--- SkyEditor.UI.WPF/NewFileWindow.xaml.vb
+++ SkyEditor.UI.WPF/NewFileWindow.xaml.vb
@@ -1,11 +1,33 @@
 ﻿Imports System.Windows
+Imports System.Windows.Controls
 
 Public Class NewFileWindow
     Inherits Window
 
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+    Private Sub btnOK_Click(sender As Object, e As RoutedEventArgs)
         DialogResult = True
         Me.Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As RoutedEventArgs)
+        DialogResult = False
+        Me.Close()
+    End Sub
+
+    Private Sub NewFileWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        UpdateBtnOKEnabled()
+    End Sub
+
+    Private Sub txtName_TextChanged(sender As Object, e As TextChangedEventArgs) Handles txtName.TextChanged
+        UpdateBtnOKEnabled()
+    End Sub
+
+    Private Sub cbType_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles cbType.SelectionChanged
+        UpdateBtnOKEnabled()
+    End Sub
+
+    Private Sub UpdateBtnOKEnabled()
+        btnOK.IsEnabled = Not String.IsNullOrEmpty(txtName.Text) AndAlso cbType.SelectedItem IsNot Nothing
     End Sub
 
     Public Property SelectedType As Type
@@ -30,5 +52,6 @@ Public Class NewFileWindow
     Public Shadows Function ShowDialog() As Boolean
         Return MyBase.ShowDialog
     End Function
+
 End Class
 
